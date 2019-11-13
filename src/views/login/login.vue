fdb1f893140721bfd988c650e23b525030bb3137
chs0320 <1113530808@qq.com>
2019-11-13 22:21:12 +0800

    黑马头条PC-登录模块-数据绑定及校验  自动校验

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -7,22 +7,22 @@
            <img src="../../assets/img/logo_index.png" alt="">
         </div>
         <!-- 表单 -->
-        <el-form>
-           <el-form-item>
-              <el-input placeholder="shenme"></el-input>
+        <el-form :model="loginForm" :rules="loginRules">
+           <el-form-item prop="mobile">
+              <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
            </el-form-item>
-           <el-form-item>
-              <el-input style="width:280px"></el-input>
+           <el-form-item prop="code">
+              <el-input v-model="loginForm.code" style="width:280px" placeholder="请输入您的验证码"></el-input>
                <!-- 提交按钮  float:right  什么东西-->
                <el-button style="float:right" plain>发送验证码</el-button>
            </el-form-item>
-            <el-from-item>
+            <el-form-item prop="checked">
                   <!-- 勾选同意框 -->
-            <el-checkbox style="margin-bottom:40px">我已阅读并同意用户协议及条款</el-checkbox>
-            </el-from-item>
-            <el-from-item>
+            <el-checkbox v-model="loginForm.checked" style="margin-bottom:40px">我已阅读并同意用户协议及条款</el-checkbox>
+            </el-form-item>
+            <el-form-item>
                <el-button style="width:100%" type='primary'>登录</el-button>
-            </el-from-item>
+            </el-form-item>
         </el-form>
      </el-card>
   </div>
@@ -30,7 +30,34 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 是否勾选协议
+      },
+      // 校验规则对象
+      loginRules: {
+        mobile: [{ required: true, message: '请输入您的手机号' }, {
+          pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号'
+        }],
+        code: [{ required: true, message: '请输入您的验证码' }, {
+          pattern: /^\d{6}$/, message: '请输入六位数字'
+        }],
+        checked: [{ validator: function (rule, value, callback) {
+          //   rule代表当前的规则
+          // value代表当前的值   表单字段checked的值
+          //  callback回调函数   是用来执行的
+          if (value) {
+            callback() // 如果是true的话 就选中了 通过校验  直接执行callback 表示直接通过
+          } else {
+            callback(new Error('你必须无条件执行')) // 要自己定义一个错误出来
+          }
+        } }]
+      }
+    }
+  }
 }
 </script>
 
