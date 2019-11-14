0be9e0784919740eb0bb3cbe0a8ccc02a5727e43
chs0320 <1113530808@qq.com>
2019-11-15 00:21:44 +0800

    黑马头条PC-主页模块-左侧导航菜单

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,41 @@
 <template>
-        <!-- 布局 -->
+  <!-- 布局 -->
   <el-container>
-        <!-- 左右布局 -->
-    <el-aside>左侧</el-aside>
-        <!-- 右边大容器 -->
-   <el-container>
-        <!-- 上边 -->
-     <el-header>头部</el-header>
-        <!-- 底下 -->
+    <!-- 左右布局 -->
+    <el-aside class="left" style="width:220px">
+      <div class="menu-title" style="background-color:#2e2f32">
+        <img src="../../assets/img/logo_admin.png" alt />
+      </div>
+      <el-menu style="width:221px" background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
+        <el-menu-item>首页</el-menu-item>
+        <el-submenu>
+          <!-- 具名插槽 -->
+          <template slot="title">内容管理</template>
+          <el-menu-item>发布文章</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>评论列表</el-menu-item>
+          <el-menu-item>素材管理</el-menu-item>
+        </el-submenu>
+        <el-submenu>
+          <template slot="title">粉丝管理</template>
+          <el-menu-item>图文数据</el-menu-item>
+          <el-menu-item>粉丝概况</el-menu-item>
+          <el-menu-item>粉丝画像</el-menu-item>
+          <el-menu-item>粉丝列表</el-menu-item>
+        </el-submenu>
+        <el-menu-item>账户信息</el-menu-item>
+      </el-menu>
+    </el-aside>
+    <!-- 右边大容器 -->
+    <el-container>
+      <!-- 上边 -->
+      <el-header>头部</el-header>
+      <!-- 底下 -->
       <el-main>
         <!-- 二级路由容器 -->
         <router-view></router-view>
       </el-main>
-   </el-container>
+    </el-container>
   </el-container>
 </template>
 
@@ -21,8 +44,22 @@
 
 export default {
   name: 'home',
-  components: {
-
-  }
+  components: {}
 }
 </script>
+<style lang="less" scoped>
+.left {
+  width: 220px;
+  height: 100vh;
+  background-color: #353b4e;
+  overflow: hidden;
+  .menu-title {
+    width: 219px;
+    padding: 10px 0;
+    text-align: center;
+    img {
+      height: 30px;
+    }
+  }
+}
+</style>
