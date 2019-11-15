b2aca1e3b010ae76ffacde220fa634a3fed794fa
chs0320 <1113530808@qq.com>
2019-11-15 23:39:49 +0800

    展示所有文章(1)请求获取文章列表数据

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -6,15 +6,15 @@
       </div>
       <el-form ref="form" label-width="80px">
         <!-- 文章状态 -->
-  <el-form-item label="文章状态">
-    <el-radio-group v-model="filterForm.status">
-      <el-radio label="全部"></el-radio>
-      <el-radio label="草稿"></el-radio>
-      <el-radio label="待审核"></el-radio>
-      <el-radio label="审核通过"></el-radio>
-      <el-radio label="审核失败"></el-radio>
-    </el-radio-group>
-  </el-form-item>
+        <el-form-item label="文章状态">
+          <el-radio-group v-model="filterForm.status">
+            <el-radio label="全部"></el-radio>
+            <el-radio label="草稿"></el-radio>
+            <el-radio label="待审核"></el-radio>
+            <el-radio label="审核通过"></el-radio>
+            <el-radio label="审核失败"></el-radio>
+          </el-radio-group>
+        </el-form-item>
         <el-form-item label="查询状态">
           <el-select placeholder="请选择" v-model="filterForm.channel_id">
             <el-option label="全部" value="shanghai"></el-option>
@@ -28,7 +28,7 @@
           <div class="block">
             <span class="demonstration"></span>
             <el-date-picker
-            v-model="rangDate"
+              v-model="rangDate"
               type="daterange"
               range-separator="-"
               start-placeholder="开始日期"
@@ -40,6 +40,37 @@
           <el-button type="primary">查询</el-button>
         </el-form-item>
       </el-form>
+    </el-card>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>共找到59806条符合条件的内容</span>
+      </div>
+        <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="发布日期">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
     </el-card>
   </div>
 </template>
@@ -54,9 +85,41 @@ export default {
         begin_pubdate: '',
         end_pubdate: ''
       },
-      // 因为时间现在只能绑一个，所以就创建了一个新的
-      rangDate: ''
+      // 因为时间现在只能绑一个，所以(必须)就创建了一个新的
+      rangDate: '',
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
+  },
+  created () {
+    const token = window.localStorage.getItem('user-token')
+    this.$axios({
+      method: 'get',
+      url: '/articles',
+      headers: {
+        // 添加请求头
+        Authorization: `Bearer ${token}`
+      }
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err, '请求失败')
+    })
   }
 }
 </script>
